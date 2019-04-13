d5151bec7f1d5fe024aedfb49029045c4d429f3a
qianfeng <pjymwR5mT[m2>
2019-04-13 19:54:20 +0800

    auto

---
+++ src/main/java/com/yqf/athl/query/BinarySearch.java
@@ -0,0 +1,22 @@
+package com.yqf.athl.query;
+
+
+import java.util.Random;
+
+/**
+ * 二分查找
+ */
+public class BinarySearch {
+
+
+    public static void main(String[] args) {
+
+
+        Random random = new Random();
+        Lists
+        random.nextInt()
+
+
+        rank(ints,ints)
+    }
+}
